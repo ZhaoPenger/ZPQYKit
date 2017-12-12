64812dfeabc1888e069c86531c86b55d31bcc7cd
zhaopeng <mrzhao90@126.com>
2017-12-12 16:18:18 +0800

    创建新的 类

---
+++ ZPKit/Class/ZYObject.h
@@ -0,0 +1,13 @@
+//
+//  ZYObject.h
+//  ZPKit
+//
+//  Created by 赵鹏 on 2017/12/12.
+//  Copyright © 2017年 赵鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZYObject : NSObject
+
+@end
---
+++ ZPKit/Class/ZYObject.m
@@ -0,0 +1,13 @@
+//
+//  ZYObject.m
+//  ZPKit
+//
+//  Created by 赵鹏 on 2017/12/12.
+//  Copyright © 2017年 赵鹏. All rights reserved.
+//
+
+#import "ZYObject.h"
+
+@implementation ZYObject
+
+@end
